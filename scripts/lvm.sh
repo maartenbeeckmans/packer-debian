45dc0e3445ac7114903f8c1c39f8c63a3c1e5ba2
Maarten Beeckmans <maartenb@inuits.eu>
2020-12-01 20:29:04 +0100

    fix lvm provisioning

--- scripts/lvm.sh
+++ scripts/lvm.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -eux
 
 echo "Removing hack lv"
-lvremove /dev/vg00/lv_hack --yes
+lvremove /dev/vg00/lv_hack --yes --autobackup y --debug --verbose --force
+
+pvdisplay
+
+vgdisplay
+
+lvdisplay
