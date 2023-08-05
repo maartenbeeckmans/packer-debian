7ecfa00e4d7068adc662388890f5013d5a4fe234
Maarten Beeckmans <maartenb@inuits.eu>
2023-08-05 17:54:09 +0200

    Remove trailing whitespace

--- scripts/motd.sh
+++ scripts/motd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
-sed -i -e "s/motd=\/run\/motd.dynamic/motd=\/etc\/motd/g" /etc/pam.d/sshd 
+sed -i -e "s/motd=\/run\/motd.dynamic/motd=\/etc\/motd/g" /etc/pam.d/sshd
 sed -i -e "s/motd=\/run\/motd.dynamic/motd=\/etc\/motd/g" /etc/pam.d/login
 
 cat << EOF > /etc/motd
